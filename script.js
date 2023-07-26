184825608d1a613b9e992341b1e17d53404237b4
barvax <69000340+barvax@users.noreply.github.com>
2023-07-26 23:21:43 +0300

    1

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Sample divs data (you can load these from your backend or generate them dynamically)
 let divData = [
-  { id: 1, content: 'Div 1' },
-  { id: 2, content: 'Div 2' },
-  { id: 3, content: 'Div 3' },
+  { id: 1, content: 'Div 1' ,index:1},
+  { id: 2, content: 'Div 2',index:2 },
+  { id: 3, content: 'Div 3' ,index:3},
   // Add more divs as needed
 ];
 
-function createDivs() {
-  const gridContainer = document.getElementById('gridContainer');
+function reIndexDivs(){
+  let index = 1;
+  divData.forEach(item => {
+    item.index= index++;
+  });
+}
 
+function createDivs() {
+  reIndexDivs();
+  const gridContainer = document.getElementById('gridContainer');
+  gridContainer.innerHTML=null
   divData.forEach(item => {
     const div = createDivItem(item);
     gridContainer.appendChild(div);
@@ -18,7 +26,11 @@ function createDivs() {
 function createDivItem(item) {
   const div = document.createElement('div');
   div.className = 'gridItem';
-  div.textContent = item.content;
+  div.textContent = item.index;
+  const h2 = document.createElement('h2');
+  h2.className = 'item_h2';
+  div.appendChild(h2);
+  h2.innerHTML = item.content;
 
   // Set the unique ID for each div element
   div.setAttribute('data-id', item.id);
@@ -31,12 +43,6 @@ function createDivItem(item) {
   });
   div.appendChild(deleteButton);
 
-  // Enable drag-and-drop for each div
-  div.draggable = true;
-  div.addEventListener('dragstart', handleDragStart);
-  div.addEventListener('dragover', handleDragOver);
-  div.addEventListener('drop', handleDrop);
-
   return div;
 }
 
@@ -45,59 +51,25 @@ function deleteDiv(id) {
   const gridContainer = document.getElementById('gridContainer');
   gridContainer.innerHTML = '';
   createDivs();
+  console.log(divData)
 }
 
 function addDiv() {
   const newDivId = divData.length + 1;
   const newDivContent = 'Div ' + newDivId;
-  const newItem = { id: newDivId, content: newDivContent };
+  const newItem = { id: newDivId, content: newDivContent,index:divData.length+1 };
   divData.push(newItem);
 
-  const gridContainer = document.getElementById('gridContainer');
-  const newDiv = createDivItem(newItem);
-  gridContainer.appendChild(newDiv);
-}
-
-// Drag and drop handlers
-let draggedElement;
-
-function handleDragStart(event) {
-  draggedElement = event.target;
-  event.dataTransfer.setData('text/plain', event.target.dataset.id);
-}
-
-function handleDragOver(event) {
-  event.preventDefault();
-}
-
-function handleDrop(event) {
-  event.preventDefault();
-  const dropTarget = event.target;
-
-  // If the drop target is the same as the dragged element, do nothing
-  if (draggedElement === dropTarget) {
-    return;
-  }
-
-  const dropTargetIndex = Array.from(dropTarget.parentNode.children).indexOf(dropTarget);
-  const draggedElementIndex = Array.from(draggedElement.parentNode.children).indexOf(draggedElement);
-
-  // Remove the dragged element from the divData array
-  const draggedItem = divData.splice(draggedElementIndex, 1)[0];
-
-  // Insert the dragged item at the new position
-  divData.splice(dropTargetIndex, 0, draggedItem);
-
-  // Re-create the div elements based on the updated divData array
-  const gridContainer = document.getElementById('gridContainer');
-  gridContainer.innerHTML = '';
+  // const gridContainer = document.getElementById('gridContainer');
+  // const newDiv = createDivItem(newItem);
+  // gridContainer.appendChild(newDiv);
+  console.log(divData)
+ 
   createDivs();
 }
 
-
-// Call the function to create the initial div elements on page load
 createDivs();
 
-// Add event listener to the "Add Div" button
+
 const addButton = document.getElementById('addButton');
 addButton.addEventListener('click', addDiv);
---
+++ sideMenu.js
@@ -0,0 +1,6 @@
+const openMenuButton = document.getElementById('openMenuButton');
+const sideMenu = document.getElementById('sideMenu');
+
+openMenuButton.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+});
