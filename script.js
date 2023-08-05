a5edcef5481d8c10980fe93756742866d0feb35f
barvax <69000340+barvax@users.noreply.github.com>
2023-08-05 12:37:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Sample divs data (you can load these from your backend or generate them dynamically)
 let divData = [
-  { id: 1, actionName: 'no Action',observation:'' ,time:'xx:xx',index:1},
-  { id: 2, actionName: 'no action',observation:'',time:'xx:xx',index:2 },
-  { id: 3, actionName: 'no action' ,observation:'',time:'xx:xx',index:3},
+  { id: 1, actionName: 'Action 1',instractions:"add at Tr>10 ",observation:'' ,time:'xx:xx',index:1,material:null,materialMass:'',sample:{hasSample:false,sampleNum:''}},
+  { id: 2, actionName: 'Action 2',instractions:"stirr for 2 hr",observation:'',time:'xx:xx',index:2,material:null,materialMass:'',sample:{hasSample:false,sampleNum:''} },
+  { id: 3, actionName: 'Action 3' ,instractions:"cooling to 0",observation:'',time:'xx:xx',index:3,material:null,materialMass:'',sample:{hasSample:false,sampleNum:''}},
   // Add more divs as needed
 ];
 function getCurrentTime() {
@@ -11,11 +11,25 @@ function getCurrentTime() {
   const minutes = now.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
 }
-
+const materials = [
+  { materialName: 'Material A', mw: 100.5 ,mass:30},
+  { materialName: 'Material B', mw: 85.2,mass:45 },
+  { materialName: 'Material C', mw: 120.7 ,mass:200},
+  { materialName: 'Material D', mw: 68.9 ,mass:300},
+  { materialName: 'Material E', mw: 150.3,mass:100 },
+  { materialName: 'Material F', mw: 72.1 ,mass:140},
+  { materialName: 'Material G', mw: 90.6 ,mass:22.12},
+  { materialName: 'Material H', mw: 110.2 ,mass:10},
+  { materialName: 'Material I', mw: 95.8 ,mass:0.3},
+  { materialName: 'Material G', mw: 78.4,mass:55 },
+];
 const currentTime = getCurrentTime();
-
-
+let isAction =true;
+let rightSectionInstractions =  document.getElementById('right-section-instractions');
+const materialNameForm = document.getElementById('Material-name-form');
+const addOMaterialInput = document.getElementById('addOMaterialInput');
 const popup = document.getElementById('popup');
+const popupSample = document.getElementById('popupSample');
 ID=4;
 tempItemId='';
 
@@ -49,11 +63,11 @@ function createDivItem(item) {
 
 
   <img src="circle-check-regular.svg" alt="Image 1">
-  <h4>Instractions</h4>
+  <h4>${item.instractions}</h4>
   <h6 class="observations-popUp">${item.observation}</h6>
   <div class="bottom-content">
     <img src="results.svg" alt="Image 2" class="action-box-bottom-img">
-    <img src="vial-solid.svg" alt="Image 3"  class="action-box-bottom-img">
+    <img src="vial-solid.svg" alt="Image 3" onClick='createSample(${item.id})'  class="action-box-bottom-img">
     <h5>${item.time}</h5>
   </div>
 </div>`
@@ -95,7 +109,7 @@ function deleteDiv(id) {
 function addDiv() {
   const newDivId = ID++;
   const newDivContent = 'Enter Action ' ;
-  const newItem = { id: newDivId, actionName: newDivContent,observation:'',index:divData.length+1,time:'xx:xx'};
+  const newItem = { id: newDivId, actionName: newDivContent,observation:'',instractions:"",index:divData.length+1,time:'xx:xx',material:null,materialMass:'',sample:{hasSample:false,sampleNum:''}};
   divData.push(newItem);
 
   // const gridContainer = document.getElementById('gridContainer');
@@ -106,6 +120,14 @@ function addDiv() {
   createDivs();
 }
 
+
+function createSample(itemID){
+  const item = divData.find((item) => item.id === itemID);
+  if (item) {
+    OpenSamplePopup();
+   
+  }
+}
 function addActionName(itemID, newName) {
   const item = divData.find((item) => item.id === itemID);
   if (item) {
@@ -128,18 +150,39 @@ function closePopUp(){
   popup.style.display ='none';
 }
 
+function OpenSamplePopup(itemId){
+  popupSample.style.display ='block';
+}
+function CloseSamplePopup(itemId){
+  popupSample.style.display ='none';
+}
 
 function OpenActionPopup(itemId){
   popup.style.display ='flex';
   let actionIndex = document.getElementById('popUpIndexNumber');
   let popUpActioTitle = document.getElementById('popUpActioTitle');
-  
+  generateActionList(30);
   const item = divData.find((item) => item.id === itemId);
   if (item) {
     tempItemId=itemId
     console.log(itemId)
     actionIndex.innerHTML=itemId;
-    popUpActioTitle.innerHTML = item.actionName;
+    addActionInput.value = item.actionName;
+    if(item.material==''){
+popUpActioTitle.innerHTML = item.actionName;
+    }else{
+      popUpActioTitle.innerHTML = item.material;
+    }
+   
+    rightSectionInstractions.innerHTML = item.instractions;
+    if(item.material!=null){
+      materialNameForm.innerHTML=item.material;
+      addOMaterialInput.value=item.materialMass;
+    }else{
+      materialNameForm.innerHTML=null;
+      addOMaterialInput.value=item.materialMass;
+    }
+   
     
    
   }
@@ -149,13 +192,10 @@ function OpenActionPopup(itemId){
 const imageElement = document.getElementById('myImage');
 imageElement.addEventListener('click', function() {
   // Call your function here
-  yourFunction();
+  materialActionToggle();
 });
 
-function yourFunction() {
-  // Your function logic here
-  console.log('Image clicked! Function called.');
-}
+
 function SubmitPopUpForm(){
   let actionInput = document.getElementById('addActionInput');
   let observationInput = document.getElementById('addObservationInput');
@@ -166,18 +206,33 @@ function SubmitPopUpForm(){
     if(actionInput.value.length>0){
       item.actionName = actionInput.value;
     }
- 
+
+ console.log(materialNameForm.innerHTML);
+   if(materialNameForm.innerHTML!=""){
+   item.material = materialNameForm.innerHTML;
+   item.actionName = item.material;
+   }else{
+    item.actionName = actionInput.value;
+   }
+   
+console.log(item.material)
+    
+  
+    item.materialMass = addOMaterialInput.value;
+    
    item.observation = observationInput.value
    item.time = getCurrentTime();
   }
   closePopUp();
   createDivs();
+ 
 }
 
 
 function generateActionList(numberOfActions) {
   const actionListElement = document.getElementById('action-list-items');
-
+  actionListElement.innerHTML=null;
+  isAction=true;
   for (let i = 1; i <= numberOfActions; i++) {
     const actionItem = document.createElement('li');
     actionItem.textContent = `Action ${i}`;
@@ -189,7 +244,34 @@ function generateActionList(numberOfActions) {
       popUpActioTitle.innerHTML = actionItem.textContent;
     })
   }
+  
 }
 
+function generateMaterialList() {
+  const actionListElement = document.getElementById('action-list-items');
+ 
+  actionListElement.innerHTML=null;
+  isAction=false;
+  for (let i = 0; i <= materials.length-1; i++) {
+    const actionItem = document.createElement('li');
+    actionItem.textContent = materials[i].materialName+" "+"..... "+materials[i].mass+" gr";
+    actionListElement.appendChild(actionItem);
+    actionItem.addEventListener("click",()=>{
+      materialNameForm.innerHTML=materials[i].materialName;
+      addOMaterialInput.value = materials[i].mass;
+     
+    })
+  }
+
+}
+function materialActionToggle(){
+if(isAction){
+ 
+  generateMaterialList();
+ return;
+}
+  generateActionList(30)
+}
 // Call the function with the desired number of actions (e.g., 30)
 generateActionList(30);
+
